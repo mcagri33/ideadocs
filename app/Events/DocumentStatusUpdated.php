de42541e951c1eafb7f888018cd9333459f045da
cagri <mehmetcagrikaratas@gmail.com>
2023-08-17 08:45:21 +0300

    TODO: Status changed but email has been failed. i will check later

--- app/Events/DocumentStatusUpdated.php
+++ app/Events/DocumentStatusUpdated.php
@@ -2,11 +2,8 @@
 
 namespace App\Events;
 
-use Illuminate\Broadcasting\Channel;
-use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PresenceChannel;
-use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use App\Models\Documents;
+use App\Models\User;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
@@ -14,12 +11,14 @@ class DocumentStatusUpdated
 {
   use Dispatchable, SerializesModels;
 
-  public $user;
   public $document;
+  public $user;
+  public $newStatus;
 
-  public function __construct($user, $document)
+  public function __construct(Documents $document, User $user, $newStatus)
   {
-    $this->user = $user;
     $this->document = $document;
+    $this->user = $user;
+    $this->newStatus = $newStatus;
   }
 }
